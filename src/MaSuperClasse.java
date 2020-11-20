3218c507c765f7784fe43010ba74c8585349cff5
erwan-couteau <erteau@aol.com>
2020-11-20 15:26:38 +0100

    2eme commit avec javadoc

---
+++ src/MaSuperClasse.java
@@ -0,0 +1,18 @@
+
+public class MaSuperClasse {
+    private String truc;
+    /**
+    * constructeur
+     */
+    public MaSuperClasse(String truc) {
+        this.truc=truc;
+    }
+
+    /**
+     * Fonction qui renvoie un truc
+     * @return
+     */
+    public String getTruc() {
+        return truc;
+    }
+}
---
+++ src/classTest.java
@@ -0,0 +1,7 @@
+public class classTest {
+    public static void main(String[] args){
+        System.out.println("yo");
+        MaSuperClasse msc = new MaSuperClasse( "whoaouh");
+        System.out.println(msc.getTruc());
+    }
+}
